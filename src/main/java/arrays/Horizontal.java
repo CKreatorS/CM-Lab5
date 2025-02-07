64083c5294b6f3b151420665be627382b6ac48c3
cmorales <caitlyn.morales424@gmail.com>
2025-02-07 10:58:10 -0500

    horizontal and vertical

---
+++ src/main/java/arrays/Horizontal.java
@@ -0,0 +1,29 @@
+package main.java.arrays;
+
+public class Horizontal {
+
+    public static void main(String[] args){ 
+        String [][] stripes = new String [11][11]; 
+
+        for(int x = 0; x < stripes.length; x++){
+            for(int y = 0; y < stripes[x].length; y++){
+                if(x % 2 == 0){
+                    stripes[x][y] = "🐚"; 
+                } else 
+                    stripes[x][y] = "🦀";
+                }
+            }
+            print2d(stripes); 
+        }
+
+
+    public static void print2d(String[][] array){
+
+        for(int x = 0; x < array.length; x++){
+            for(int y = 0; y < array[x].length; y++){
+                System.out.print(array[x][y]); 
+            }
+            System.out.println(); 
+        }
+    }
+}
