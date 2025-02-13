efcf9c66ab472acad0beb589d20b1e92e8196c71
cmorales <caitlyn.morales424@gmail.com>
2025-02-13 08:54:49 -0500

    argyle half

--- src/main/java/arrays/Argyle.java
+++ src/main/java/arrays/Argyle.java
@@ -3,22 +3,26 @@ package main.java.arrays;
 public class Argyle {
 
     public static void main(String[] args) {
-        String [][] ecks = new String [11][11];
+        String [][] argyle = new String [11][11];
 
-        for(int x = 0; x < ecks.length; x++){
-            for(int y = 0; y < ecks[x].length; y++){
+        for(int x = 0; x < argyle.length; x++){
+            for(int y = 0; y < argyle[x].length; y++){
                 if (x == y) {
-                    ecks[x][y] = "🦀";
+                    argyle[x][y] = "🦀";
                 }
-                if (x == y){
-                    ecks[x][y] = "🦀";
-                }    
-                else{
-                    ecks[x][y] = "🐚";
-                } 
+                else if (x + y == argyle.length - 1){
+                    argyle[x][y] = "🦀";
+                }
+                else if(x <= argyle.length/2){
+                    argyle[x][argyle.length/2 + x] = "🦀";
+                    argyle[x][argyle.length/2 - x] = "🦀";
+                }
+                else {
+                    argyle[x][y] = "🐚";
+                }
             }
         }
-        print2d(ecks);
+        print2d(argyle);
     }
 
     public static void print2d(String[][] array){
